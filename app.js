2c3719c6dcb6f8736260f9c17861ec04eca2669f
lexyking <sahiealexandre@hotmail.fr>
2019-05-10 00:49:35 +0100

    javascript files

--- app.js
+++ app.js
@@ -6,4 +6,27 @@ const cartOverlay = document.querySelector('.cart-overlay');
 const cartItems = document.querySelector('.cart-items');
 const cartTotal = document.querySelector('.cart-total');
 const cartContent = document.querySelector('.cart-content');
-const productsDOM = document.querySelector('.cart-content');
+const productsDOM = document.querySelector('.products-center');
+
+let cart = [];
+
+//getting the products
+class Products {
+
+}
+
+//Display products
+class UI {
+
+}
+
+//local storage
+class Storage {
+
+}
+
+document.addEventListener('DOMContentLoadded', () => {
+    const ui = new UI();
+    const products = new Products();
+
+})
